c22bb5fdb2fce3c4616ac672bf5e86cc777fffec
tuvaco <tuvaco@stud.ntnu.no>
2021-09-16 10:37:53 +0200

    Laget user-klasse

---
+++ Smittesporer/core/src/main/java/smittesporer/User.java
@@ -0,0 +1,41 @@
+package smittesporer;
+
+private String name, email, password;
+
+public class User {
+
+    public User (String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public static void main(String[] args) {
+        User newUser = new User("tuva", "tuvaco@stud.ntnu.no", "ntnu");
+    }
+
+}
