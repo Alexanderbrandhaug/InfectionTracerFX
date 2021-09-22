ef9d2d581ed8be5bcdc28a4f705b89c39769a093
Alexander <alexabr@stud.ntnu.no>
2021-09-22 14:16:31 +0200

    deleted a folder that we do not know off

--- Smittesporer/src/main/java/gr2181/smittesporer/App.java
+++ Smittesporer/src/main/java/gr2181/smittesporer/App.java
@@ -6,26 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    @Override
+	@Override
 	public void start(final Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Smittesporer");
 		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("login.fxml"))));
 		primaryStage.show();
-		
-		
-		
-	}
-	
 
-	
+	}
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-	}
+}
--- Smittesporer/src/main/java/gr2181/smittesporer/LoginController.java
+++ Smittesporer/src/main/java/gr2181/smittesporer/LoginController.java
@@ -1,6 +1,5 @@
 package gr2181.smittesporer;
 
-
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -9,36 +8,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class LoginController {
 
-private Stage window1 = new Stage();
-    
-    
-
-    
+    private Stage window1 = new Stage();
 
     @FXML
     void loginBtn(ActionEvent event) {
-        
-  
+
         try {
-            
+
             window1.setScene(new Scene(FXMLLoader.load(getClass().getResource("main.fxml"))));
             window1.show();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-  
+
     }
 
     @FXML
     void registerBtn(ActionEvent event) {
-           
-  
+
         try {
-            
+
             window1.setScene(new Scene(FXMLLoader.load(getClass().getResource("registration.fxml"))));
             window1.show();
         } catch (IOException e) {
