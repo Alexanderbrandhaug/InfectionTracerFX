f5a40d9e9a59d7f0d2aa39310e200d876dbe9fed
tuvaco <tuvaco@stud.ntnu.no>
2021-09-22 10:47:30 +0200

    Now working as intended

--- Smittesporer/src/main/java/gr2181/smittesporer/LoginController.java
+++ Smittesporer/src/main/java/gr2181/smittesporer/LoginController.java
@@ -33,4 +33,19 @@ private Stage window1 = new Stage();
   
     }
 
+    @FXML
+    void registerBtn(ActionEvent event) {
+           
+  
+        try {
+            
+            window1.setScene(new Scene(FXMLLoader.load(getClass().getResource("registration.fxml"))));
+            window1.show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
 }
