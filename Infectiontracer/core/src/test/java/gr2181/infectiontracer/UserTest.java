a385a74e732d091524173f45b4ec8071f66b0269
Alexander <alexabr@stud.ntnu.no>
2021-10-08 11:41:44 +0200

    re-worked logically structure in User and maincontroller, deleted CloseContact class(not needed), added more fields that are now displayed in tableview

--- Infectiontracer/core/src/test/java/gr2181/infectiontracer/UserTest.java
+++ Infectiontracer/core/src/test/java/gr2181/infectiontracer/UserTest.java
@@ -10,46 +10,46 @@ public class UserTest {
 
     @BeforeEach
     public void setup() {
-        user = new User("Ola", "Nordmann", "test@gmail.com", "Passord321!");
+        user = new User("Ola", "Nordmann", "test@gmail.com", "Passord321!", false, "");
     }
 
     @Test
     public void testConstructor() {
         // test for specialchar in lastname
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new User("Alex", "@Test", "test@gmail.com", "Passord321");
+            new User("Alex", "@Test", "test@gmail.com", "Passord321", false, "");
         });
         // test for email without a @
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new User("Alex", "Test", "testgmail.com", "Passord321");
+            new User("Alex", "Test", "testgmail.com", "Passord321", false, "");
         });
         // test for email without a dot
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new User("Alex", "Nordmann", "test@gmailcom", "Passord321");
+            new User("Alex", "Nordmann", "test@gmailcom", "Passord321", false, "");
         });
         // test for weak password <5 chars
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new User("Alex", "Nordmann", "test@gmail.com", "12345");
+            new User("Alex", "Nordmann", "test@gmail.com", "12345", false, "");
         });
         // test for numbers in forname
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new User("Alex12", "Nordmann", "test@gmail.com", "Passord321");
+            new User("Alex12", "Nordmann", "test@gmail.com", "Passord321", false, "");
         });
     }
 
     @Test
     public void testSetForname() {
-        user.setForName("Peter");
-        Assertions.assertEquals("Peter", user.getForName());
-        user.setForName("Maria");
-        Assertions.assertEquals("Maria", user.getForName());
+        user.setForname("Peter");
+        Assertions.assertEquals("Peter", user.getForname());
+        user.setForname("Maria");
+        Assertions.assertEquals("Maria", user.getForname());
         // test to see if you are able to set forName as a whitespace
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            user.setForName(" ");
+            user.setForname(" ");
         });
         // test to see if you can use numbers and whitespace
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            user.setForName("Alex 92");
+            user.setForname("Alex 92");
         });
 
     }
--- Infectiontracer/fxui/src/main/java/infectiontracer/ui/MainController.java
+++ Infectiontracer/fxui/src/main/java/infectiontracer/ui/MainController.java
@@ -12,14 +12,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import java.util.List;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.SortedMap;
 import infectiontracer.core.*;
-
-
 
 public class MainController extends AbstractController {
 
@@ -39,17 +36,21 @@ public class MainController extends AbstractController {
     private Label numberOfContacts;
 
     @FXML
-    private TableView<CloseContact> contactTable;
+    private TableView<User> contactTable;
     @FXML
-    private TableColumn<CloseContact, String> nameColumn;
-    @FXML
-    private TableColumn<CloseContact, LocalDate> DateColumn;
+    private TableColumn<User, String> nameColumn;
 
     @FXML
-    private TableColumn<CloseContact, Boolean> healthStatusColumn;
+    private TableColumn<User, String> lastnameColumn;
 
     @FXML
-    private TableColumn<CloseContact, String> dateOfInfectionColumn;
+    private TableColumn<User, String> emailColumn;
+
+    @FXML
+    private TableColumn<User, Boolean> healthstatusColumn;
+
+    @FXML
+    private TableColumn<User, String> dateOfInfectionColumn;
 
     @FXML
     private TextField contactNameTxt;
@@ -59,27 +60,27 @@ public class MainController extends AbstractController {
 
     @FXML
     private Button fireInfectedUser;
-    boolean healthStatus = false;
-    ObservableList<CloseContact> contactList = FXCollections.observableArrayList();
-    
-  //  Filehandler filehandler = new FileHandler();
+    ObservableList<User> contactList = FXCollections.observableArrayList();
+
+    // Filehandler filehandler = new FileHandler();
 
     @FXML
-    void addContact(ActionEvent event)throws IOException {
+    void addContact(ActionEvent event) throws IOException {
         try {
             infectionTracer.addCloseContact(username, contactNameTxt.getText());
-            SortedMap<String, String> currentMap = infectionTracer.getRelevantMap(username);
+            List<User> currentMap = infectionTracer.getRelevantMap(username);
             contactList.clear();
-            for (Map.Entry<String, String> entry : currentMap.entrySet()) {
-              
+            for (User user : currentMap) {
+
                 System.out.println(contactList.toString());
-                CloseContact closeContact = new CloseContact(entry.getKey(), LocalDate.parse(entry.getValue()), healthStatus, "test");
-                
+                User closeContact = new User(user.getForname(), user.getLastname(), user.getEmail(), user.getPassword(),
+                        user.getHealthStatus(), user.getDateOfInfection());
+
                 contactList.add(closeContact);
-                
-                }
-                contactTable.setItems(contactList);
-                System.out.println(contactList.toString());
+
+            }
+            contactTable.setItems(contactList);
+            System.out.println(contactList.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -87,23 +88,29 @@ public class MainController extends AbstractController {
 
     // Function to refresh tableview after every insertion of a close contact
     @FXML
-    private void initialize()throws IOException {
-        SortedMap<String, String> currentMap = infectionTracer.getRelevantMap(username);
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        DateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-        healthStatusColumn.setCellValueFactory(new PropertyValueFactory<>("infected"));
-        dateOfInfectionColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfInfected"));
+    private void initialize() {
+        try {
+
+            List<User> currentMap = infectionTracer.getRelevantMap(username);
+
+            nameColumn.setCellValueFactory(new PropertyValueFactory<>("forname"));
+            lastnameColumn.setCellValueFactory(new PropertyValueFactory<>("lastname"));
+            emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+            healthstatusColumn.setCellValueFactory(new PropertyValueFactory<>("healthStatus"));
+            dateOfInfectionColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfInfection"));
             if (currentMap == null) {
                 return;
             }
-            
-            for (Map.Entry<String, String> entry : currentMap.entrySet()) {
-                //user.setEmail(entry.getKey());
-                
-                CloseContact closeContact = new CloseContact(entry.getKey(), LocalDate.parse(entry.getValue()),healthStatus,"test");
+            for (User user : currentMap) {
+                // user.setEmail(entry.getKey());
+                User closeContact = new User(user.getForname(), user.getLastname(), user.getEmail(), user.getPassword(),
+                        user.getHealthStatus(), user.getDateOfInfection());
                 contactList.add(closeContact);
                 contactTable.setItems(contactList);
-    }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-}
+    }
 }
