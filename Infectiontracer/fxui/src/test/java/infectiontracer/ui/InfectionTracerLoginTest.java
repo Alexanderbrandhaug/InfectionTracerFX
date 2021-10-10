6b0cf01f6211853d14722b1075391068f0b65f1c
Alexander <alexabr@stud.ntnu.no>
2021-10-10 12:21:36 +0200

    valid and invalid tests now work, still need to be better tested

--- Infectiontracer/fxui/src/test/java/infectiontracer/ui/InfectionTracerLoginTest.java
+++ Infectiontracer/fxui/src/test/java/infectiontracer/ui/InfectionTracerLoginTest.java
@@ -14,10 +14,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import infectiontracer.ui.*;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.isDisabled;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
+import static org.testfx.matcher.base.NodeMatchers.isInvisible;
+import static org.testfx.matcher.base.NodeMatchers.isNotNull;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+import static org.testfx.util.DebugUtils.informedErrorMessage;
 
 public class InfectionTracerLoginTest extends ApplicationTest {
 
     private ScreenController screencontroller = new ScreenController();
+    // private MainController maincontroller = new MainController();
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -28,12 +37,24 @@ public class InfectionTracerLoginTest extends ApplicationTest {
     }
 
     @Test
-    public void testInvalidLogin() {
+    public void testValidLogin() {
         String username = "test@gmail.com";
         String password = "Passord123";
         clickOn("#email_txt").write(username);
         clickOn("#password_txt").write(password);
-        clickOn("#loginBtn");
+        clickOn("#loginBtnID");
+
+    }
+
+    @Test
+    void testInvalidLogin() {
+
+        String username = "test";
+        String password = "";
+        clickOn("#email_txt").write(username);
+        clickOn("#password_txt").write(password);
+        clickOn("#loginBtnID");
+        verifyThat("OK", isVisible());
 
     }
 }
