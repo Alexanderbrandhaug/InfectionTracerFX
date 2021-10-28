66a5fc1301cc079de4a4595f12581327c91ebc03
Alexander Brandhaug <alexabr@stud.ntnu.no>
2021-10-28 09:02:04 +0200

    added springboot dependency and initializer class

--- Infectiontracer/rest/src/main/java/module-info.java
+++ Infectiontracer/rest/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module infectiontracer.rest {
    requires infectiontracer.core;
+   requires java.annotation;
+   requires spring.beans;
+   requires spring.context;
+   requires spring.web;
+   requires spring.boot;
+   requires spring.boot.autoconfigure;
+  
+   exports infectiontracer.restserver;
 
-   
 
-    opens infectiontracer.restapi;
 }
