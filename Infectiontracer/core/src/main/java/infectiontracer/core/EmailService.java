d8c334f5a4b4aaacc6396e3b3a646abaa3ef55f1
Alexander <alexabr@stud.ntnu.no>
2021-11-19 15:43:27 +0100

    changed javax lib to jakarta, in order for jlink to work. Also commented out the use of rest module in fxui module.

--- Infectiontracer/core/src/main/java/infectiontracer/core/EmailService.java
+++ Infectiontracer/core/src/main/java/infectiontracer/core/EmailService.java
@@ -1,12 +1,9 @@
 package infectiontracer.core;
-import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import java.util.*;
+import java.io.*;
+import jakarta.mail.*;
+import jakarta.mail.event.*;
+import jakarta.mail.internet.*;
 import java.util.List;
 
 
